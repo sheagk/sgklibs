d24f68cfc4045c0226e058df0c0047308a98e639
Shea Garrison-Kimmel <sheagk@gmail.com>
2019-03-29 13:51:58 -0700

    forgot to commit the change to low_level_utils as well

--- low_level_utils.py
+++ low_level_utils.py
@@ -13,3 +13,11 @@ def isfloat(string):
         return True
     except ValueError:
         return False
+
+
+def normalize_array(ar):
+    shifted_ar = ar - ar.min()
+    if shifted_ar.max() == 0:
+        return np.zeros(shifted_ar.shape)
+    else:
+        return shifted_ar/shifted_ar.max()
