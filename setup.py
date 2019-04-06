b80fdebd769201ba8393b10cb6b56c28b17364ff
Shea Garrison-Kimmel <sheagk@gmail.com>
2019-04-05 22:56:27 -0700

    turning this into a package.  need to add docs next.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='sgklibs',
+      version='0.1',
+      description="Various tools and utilities for analyzing and visualizing particle-based simulations",
+      url="https://github.org/sheagk/sgklibs",
+      author='Shea Garrison-Kimmel',
+      author_email='sheagk@gmail.com',
+      license='MIT',
+      packages=find_packages(), 
+      zip_safe=False)
